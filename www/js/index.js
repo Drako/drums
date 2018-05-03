b0e9b75361fb2614088ec0335717ee071d7c3671
Jevgeni Mensenin <jevgeni.mensenin@gmail.com>
2018-05-03 23:21:23 +0200

    :sparkles: loading sounds, randomly playing on enter press. TODO refactor.

--- www/js/index.js
+++ www/js/index.js
@@ -36,9 +36,37 @@ class App {
 
         this.sprites = {};
 
+        // PIXI.loader
+        //     .add('img/drums.jpg')
+        //     .add('sound-1', 'sounds/1.wav')
+        //     .add('sound-2', 'sounds/2.wav')
+        //     .add('sound-3', 'sounds/3.wav')
+        //     .add('sound-4', 'sounds/4.wav')
+        //     .add('sound-5', 'sounds/5.wav')
+        //     .load(this.onAssetsReady.bind(this));
+
+        //TODO refactor (can we use multiple loaders?)
         PIXI.loader
-            .add('img/drums.jpg')
-            .load(this.onAssetsReady.bind(this));
+            .add('sounds/1.wav')
+            .add('sounds/2.wav')
+            .add('sounds/3.wav')
+            .add('sounds/4.wav')
+            .add('sounds/5.wav')
+            .add('sounds/6.wav')
+            .add('sounds/7.wav')
+            .add('sounds/8.wav')
+            .add('sounds/9.wav')
+            .add('sounds/10.wav')
+            .add('sounds/11.wav')
+            .add('sounds/12.wav')
+            .add('sounds/13.wav')
+            .add('sounds/14.wav')
+            .add('sounds/15.wav')
+            .add('sounds/16.wav')
+            .add('sounds/17.wav')
+            .add('sounds/18.wav')
+            .add('sounds/19.wav')
+            .load(this.mapSomeSounds.bind(this));
     }
 
     onAssetsReady() {
@@ -48,6 +76,20 @@ class App {
         this.resizeDrums();
 
         window.addEventListener('optimizedResize', this.onResize.bind(this), false);
+    }
+
+    //TODO refactor
+    mapSomeSounds(loader, resources) {
+        debugger;
+        document.addEventListener('keypress', (event) => {
+            console.log(event.code)
+            if (event.code === 'Enter') {
+                let sounds = Object.values(resources)
+                const randomIndex = Math.floor(Math.random() * sounds.length)
+                const sound = sounds[randomIndex].data;
+                sound.play();
+            }
+        });
     }
 
     onResize() {
